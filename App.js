152557b699d989aa879be530ca8d1db8009a4619
Alok-Gahlot <alok.gahlot@appinventiv.com>
2021-10-25 11:39:20 +0530

    Git part check

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ function App() {
   const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
   Sentry.init({
     dsn: 'https://0505d3aa4a9043c99031230202bf40b7@o1045193.ingest.sentry.io/6025118',
+    environment: 'production',
     integrations: [
       new Sentry.ReactNativeTracing({
         tracingOrigins: ['localhost', 'my-site-url.com', /^\//],
@@ -14,6 +15,8 @@ function App() {
     ],
     tracesSampleRate: 0.2,
   });
+
+  throw new Error('Git check');
 
   return <Navigation routingInstrumentation={routingInstrumentation} />;
 }
