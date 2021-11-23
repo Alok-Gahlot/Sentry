c53b57b885cbb2b417c55d29137c07f58eb17500
Alok-Gahlot <alok.gahlot@appinventiv.com>
2021-11-23 14:32:35 +0530

    new chnage

--- src/NextScreen.js
+++ src/NextScreen.js
@@ -3,6 +3,17 @@ import React from 'react';
 import * as Sentry from '@sentry/react-native';
 import {StyleSheet, Text, View, Button} from 'react-native';
 
+const nextScreenNavigation = () => {
+  const transaction = Sentry.startTransaction({name: 'nextScreenNavigation'});
+
+  console.log('nextScreenNavigation');
+
+  Sentry.getCurrentHub().configureScope(scope => scope.setSpan(transaction));
+  const span = transaction.startChild({
+    func() {},
+  });
+};
+
 export default function Home({navigation}) {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
