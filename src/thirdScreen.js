bc408f8dff5d1d64fa41772178c24339535dc918
Alok-Gahlot <alok.gahlot@appinventiv.com>
2021-11-24 11:35:22 +0530

    api implemented

--- src/thirdScreen.js
+++ src/thirdScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, FlatList, Image, StyleSheet} from 'react-native';
 import * as Sentry from '@sentry/react-native';
 const func = () => {
   console.log('third');
@@ -7,6 +7,8 @@ const func = () => {
     return 100;
   }, 3000);
 };
+
+const str = 'https://image.tmdb.org/t/p/original';
 
 const anotherfunc = val => {
   return true;
@@ -37,7 +39,29 @@ const shopCheckout = () => {
   }
 };
 
+const renderItem = ({item}) => {
+  return (
+    <Image
+      style={styles.imageSty}
+      source={{uri: str.concat(item.poster_path)}}
+      resizeMode="center"
+    />
+  );
+};
+
 export default function thirdScreen({navigation}) {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/discover/tv?api_key=88fe02d450f4e58ea616b42de9b0d0e7&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false&with_watch_monetization_types=flatrate',
+    )
+      .then(response => response.json())
+      .then(json => {
+        setData(json.results);
+      })
+      .catch(error => console.error(error));
+  }, []);
+  console.log(data);
   return (
     <View>
       <Text>THird scren</Text>
@@ -49,6 +73,19 @@ export default function thirdScreen({navigation}) {
           navigation.navigate('Home');
         }}
       />
+
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(_, index) => index}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  imageSty: {
+    width: '100%',
+    height: 300,
+  },
+});
